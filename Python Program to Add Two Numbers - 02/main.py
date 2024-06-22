48146160795aa7be7710fc2b51ed6cf8a25539cc
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-06-23 00:37:09 +0530

    Python Program to Add Two Numbers

---
+++ Python Program to Add Two Numbers - 02/main.py
@@ -0,0 +1,14 @@
+# Method 01
+number1 = 1
+number2 = 2
+
+number3 = number1 + number2
+print("Sum is ",number3)
+
+# Method 02
+user_input1 = int(input("Enter number 01: "))
+user_input2 = int(input("Enter number 02: "))
+
+sum = user_input1 + user_input2
+
+print('Sum is ',sum)
